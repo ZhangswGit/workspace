f2fdadde02370fcc0ba06148fce6a39489a0ebab
bob <bob@localhost>
2020-06-09 20:13:43 +0800

    commit by 添加解析excel,生成文件

---
+++ src/main/java/com/ultrapower/demo/utils/FileUpdate2.java
@@ -0,0 +1,89 @@
+package com.ultrapower.demo.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.ResourceUtils;
+
+public class FileUpdate2 {
+	public static void main(String[] args) {
+		try {
+			String fileName = "aggregate_min_ryh_08_01ng1-golden";
+			List<String> fileNames = fileNames();
+			String path = File.separator + "Users" + File.separator + "bob" + File.separator + "Downloads" + File.separator
+					+ "QQ文件" + File.separator + fileName + ".csv";
+			String pathroot = File.separator + "Users" + File.separator + "bob" + File.separator + "Downloads" + File.separator
+					+ "QQ文件" + File.separator +  fileName + "new" + File.separator ;
+			for (String filename : fileNames) {
+				copyFileUsingFileStreams(new File(path), new File(pathroot + filename));
+			}
+			System.out.println("完成");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static List<String> fileNames() {
+		List<String> fileNames = new ArrayList<String>();
+		try {
+			// 定义一个数据格式化对象
+			XSSFWorkbook wb = null;
+			// 获取sheet表格，及读取单元格内容
+			XSSFSheet sheet = null;
+			// excel模板路径
+//			File cfgFile = ResourceUtils.getFile("D:" + File.separator + "临时文件" + File.separator + "复制" + File.separator
+//					+ "0609" + File.separator + "XC-#9-支持GB18030编码格式-ryh-V1.1.xlsx");
+			File cfgFile = ResourceUtils.getFile(
+					File.separator + "Users" + File.separator + "bob" + File.separator + "Downloads" + File.separator
+					+ "QQ文件" + File.separator + "XC-#9-支持GB18030编码格式-ryh-V1.1.xlsx");
+			InputStream in = new FileInputStream(cfgFile);
+			// 读取excel模板
+			wb = new XSSFWorkbook(in);
+			sheet = wb.getSheetAt(0);
+			// 先将获取的单元格设置为String类型，下面使用getStringCellValue获取单元格内容
+			// 如果不设置为String类型，如果单元格是数字，则报如下异常
+			// java.lang.IllegalStateException: Cannot get a STRING value from a
+			// NUMERIC cell
+			for (int i = 1; i < (1 << 9); i++) {
+				XSSFRow row = sheet.getRow(i);
+				if (row == null) {
+					break;
+				}
+				XSSFCell cell = row.getCell(5);
+				// 读取单元格内容
+				String cellValue = cell.getStringCellValue();
+				fileNames.add(cellValue + "ng2-golden" + ".csv");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fileNames;
+	}
+	
+	private static void copyFileUsingFileStreams(File source, File dest) throws IOException {
+		InputStream input = null;
+		OutputStream output = null;
+		try {
+			input = new FileInputStream(source);
+			output = new FileOutputStream(dest);
+			byte[] buf = new byte[1024];
+			int bytesRead;
+			while ((bytesRead = input.read(buf)) > 0) {
+				output.write(buf, 0, bytesRead);
+			}
+		} finally {
+			input.close();
+			output.close();
+		}
+	}
+}
